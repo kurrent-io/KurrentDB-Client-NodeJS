a6dd89bb0825a93aaa091737571911b98e69701b
YoEight <yo.eight@gmail.com>
2025-06-18 09:11:37 -0400

    more plugging stuff

--- packages/db-client/src/streams/appendToStream/multi.ts
+++ packages/db-client/src/streams/appendToStream/multi.ts
@@ -1,6 +1,10 @@
 import {AppendStreamRequest, MultiAppendResult} from "../../types";
 import grpc from "../../../generated/streams.v2_grpc_pb";
 import type {Client} from "../../Client";
+import protobuf from "../../../generated/streams.v2_pb";
+import dynamic from "../../../generated/dynamic-value_pb";
+import { backpressuredWrite, InternalOptions } from "../../utils";
+import type { AppendToStreamOptions } from ".";
 
 export const multiAppend = async function (
   this: Client,
@@ -19,7 +23,56 @@ export const multiAppend = async function (
         sink.on("error", (err) => reject(err));
 
         for (const request of requests) {
+          const message = new protobuf.AppendStreamRequest();
+          message.setStream(request.streamName);
 
+          switch (request.expectedState) {
+            case "any": {
+              message.setExpectedRevision((-2).toString(10));
+              break;
+            }
+            case "no_stream": {
+              message.setExpectedRevision((-1).toString(10));
+              break;
+            }
+            case "stream_exists": {
+              message.setExpectedRevision((-4).toString(10));
+              break;
+            }
+            default: {
+              message.setExpectedRevision(request.expectedState.toString(10));
+              break;
+            }
+          }
+
+          for (const event of request.events) {
+            const record = new protobuf.AppendRecord();
+            const dataFormat = new dynamic.DynamicValue();
+            const schemaName = new dynamic.DynamicValue();
+            record.setRecordId(event.id);
+
+            dataFormat.setBytesValue(event.contentType);
+            schemaName.setBytesValue(event.type);
+            record.getPropertiesMap().set("$schema.data-format", dataFormat);
+            record.getPropertiesMap().set("$schema.name", schemaName);
+
+            switch (event.contentType) {
+              case "application/json": {
+                const data = JSON.stringify(event.data);
+                record.setData(Buffer.from(data, "utf8").toString("base64"));
+                break;
+              }
+
+              case "application/octet-stream": {
+                record.setData(event.data);
+                break;
+              }
+            }
+
+            message.addRecords(record);
+          }
+
+          await backpressuredWrite(sink, message);
         }
       }));
 }
