07164d0d38c52b3892ed6203219c336bdfacf388
YoEight <yo.eight@gmail.com>
2025-06-18 22:09:22 -0400

    add tests

---
+++ packages/test/src/streams/multiAppendStream.test.ts
@@ -0,0 +1,86 @@
+import {
+  binaryTestEvents,
+  collect,
+  createTestNode,
+  jsonTestEvents, matchServerVersion, optionalDescribe,
+} from "@test-utils";
+
+import {
+  KurrentDBClient,
+  jsonEvent,
+  WrongExpectedVersionError,
+  ANY,
+  NO_STREAM,
+  STREAM_EXISTS,
+  binaryEvent,
+  BinaryEventType,
+  JSONEventType, AppendStreamRequest, UnsupportedError,
+} from "@kurrent/kurrentdb-client";
+
+import { v4 } from "uuid";
+
+describe("appendToStream", () => {
+  const supported = matchServerVersion`>=25.0`;
+  const node = createTestNode();
+  let client!: KurrentDBClient;
+
+  beforeAll(async () => {
+    await node.up();
+    client = KurrentDBClient.connectionString(node.connectionString());
+  });
+
+  afterAll(async () => {
+    await node.down();
+  });
+
+  optionalDescribe(supported)("should successfully append to multiple streams", () => {
+    test("json events", async () => {
+      const STREAM_NAME_1 = v4().toString();
+      const STREAM_NAME_2 = v4().toString();
+
+      const requests: AppendStreamRequest[] = [];
+
+      requests.push({
+        streamName: STREAM_NAME_1,
+        events: jsonTestEvents(),
+        expectedState: ANY,
+      });
+
+      requests.push({
+        streamName: STREAM_NAME_2,
+        events: jsonTestEvents(),
+        expectedState: ANY,
+      });
+
+      const result = await client.multiAppend(requests);
+      expect(result).toBeDefined();
+      expect(result.success).toBeTruthy();
+    });
+  });
+
+  optionalDescribe(!supported)("not supported (<25.0)", () => {
+    test("throw unsupported error", async () => {
+      const STREAM_NAME_1 = v4().toString();
+      const STREAM_NAME_2 = v4().toString();
+      const requests: AppendStreamRequest[] = [];
+
+      requests.push({
+        streamName: STREAM_NAME_1,
+        events: jsonTestEvents(),
+        expectedState: ANY,
+      });
+
+      requests.push({
+        streamName: STREAM_NAME_2,
+        events: jsonTestEvents(),
+        expectedState: ANY,
+      });
+
+      try {
+        await client.multiAppend(requests);
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnsupportedError);
+      }
+    });
+  });
+});
